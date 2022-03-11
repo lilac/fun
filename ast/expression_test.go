f31eabf2e8912f11a40307f846d9eef59d3c7830
lilac <cnjamesdeng@gmail.com>
2022-03-11 23:48:10 +0800

    Fix tests

--- ast/expression_test.go
+++ ast/expression_test.go
@@ -21,7 +21,7 @@ func TestUnit(t *testing.T) {
 	unit := &Unit{HasToken{Token: tok}}
 	assert.Equal(t, unit.Start(), start)
 	assert.Equal(t, unit.End(), end, "End token should match")
-	assert.Equal(t, unit.String(), "Unit")
+	assert.Equal(t, unit.String(), "()")
 }
 
 func TestBool(t *testing.T) {
@@ -29,6 +29,6 @@ func TestBool(t *testing.T) {
 	node := &Bool{HasToken{tok}, boolValue}
 	assert.Equal(t, node.Start(), start)
 	assert.Equal(t, node.End(), end, "End token should match")
-	assert.Equal(t, node.String(), "Bool")
+	assert.Equal(t, node.String(), "true")
 	assert.Equal(t, node.Value, boolValue)
 }
